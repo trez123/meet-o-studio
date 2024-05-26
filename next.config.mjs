5cc00912b953470370122976946726d4d18e7559
Silas Coley <silascoley3@gmail.com>
2024-05-26 18:54:52 -0500

    image update

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,9 @@ import { unifiedConditional } from 'unified-conditional'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  images: {
+    unoptimized: true
+  }
 }
 
 function remarkMDXLayout(source, metaName) {
